d0edd82be0d2a65c13ccaf64d802d75f89f53733
neduglamour <rapglamour1@gmail.com>
2026-07-16 22:18:04 +0100

    Added the Footer and Navbar to the cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { CartItem, getCart } from "@/services/cartService";
 import CartItems from "@/components/cartItems";
 import CartSummary from "@/components/cartSummary";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 
 export default function cartPage() {
@@ -15,10 +17,14 @@ export default function cartPage() {
         }, []);
 
     return (
-    <main className="max-w-7xl mx-auto px-6 py-10">
-        <h1 className="text-3xl font-bold mb-8">
-        Shopping Cart
-        </h1>
+
+        <>
+
+        <Navbar/>
+        <main className="max-w-7xl mx-auto px-6 py-10">
+            {/* <h1 className="text-3xl font-bold mb-8">
+            Shopping Cart
+            </h1> */}
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
 
@@ -35,6 +41,10 @@ export default function cartPage() {
 
         </div>
     </main>
+
+    <Footer />
+
+    </>
 );
 
 }
